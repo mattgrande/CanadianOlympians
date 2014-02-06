718a3590197270cc9e609e5ecbb348c6354c62b3
Matt Grande <matt@weeverapps.com>
2014-02-05 21:19:02 -0500

    Adding lat lng handling.

--- maps.js
+++ maps.js
@@ -1,60 +1,59 @@
-$(document).ready(function() {
-	alert('ready');
-	var center = new google.maps.LatLng(56, -96);
-	var myOptions = {
-		'zoom': 3,
-		'center': center,
-		'mapTypeId': google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById('map'), myOptions);	
-	// var marker = new google.maps.Marker({
-	// 	position: center, 
-	// 	map: map, 
-	// 	title:'Post Location'
-	// });
-	alert('mapped');
+function getPinImage( count ) {
+	var scale = 1 + (count * 0.1),
+	    pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + count + "|FE7569",
+		null, null, null, new google.maps.Size(21 * scale, 34 * scale)
+	);
+	return pinImage;
+}
 
+function getPinShadow() {
+	var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+		null, null, new google.maps.Point(12, 35));
+	return pinShadow;
+}
+
+function addMarker( lat, lng ) {
+	var marker = new google.maps.Marker({
+			position: new google.maps.LatLng( lat, lng ),
+			animation: google.maps.Animation.DROP,
+			icon: pinImage,
+			shadow: pinShadow,
+			map: map
+		});
+	return marker;
+}
+
+$(document).ready(function() {
+	var center    = new google.maps.LatLng(56, -96),
+	    myOptions = {
+			'zoom': 3,
+			'center': center,
+			'mapTypeId': google.maps.MapTypeId.ROADMAP
+		},
+	    map       = new google.maps.Map(document.getElementById('map'), myOptions),
+	    geocoder  = new google.maps.Geocoder();
+	
 	$.get( "olympics.js", function(data) {
-		console.log('success');
 		var cities = JSON.parse( data );
 		for (var i = 0; i < cities.length; i++) {
-			var city = cities[i]
-			console.log( city );
+			var city      = cities[i],
+			    count     = city.athletes.length,
+			    pinImage  = getPinImage( count ),
+			    pinShadow = getPinShadow();
 
-			var count = city.athletes.length,
-			    scale = 1 + (count * 0.1);
-			console.log( 'Scale: ' + scale );
-
-			// Generate the pin's image
-			var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + count + "|FE7569",
-	            null, null, null,
-	            new google.maps.Size(21 * scale, 34 * scale)
-	        );
-
-	        // Generate the pin's shadow
-	        var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-	            null, null, new google.maps.Point(12, 35));
-
-	        var geocoder = new google.maps.Geocoder();
-		    if (geocoder) {
-				geocoder.geocode({"address": city.name}, function(results, status) {
+			if ( city.lat && city.lng ) {
+				addMarker( city.lat, city.lng );
+			} else {
+				geocoder.geocode( { "address": city.name }, function(results, status) {
 					if (status == google.maps.GeocoderStatus.OK) {
-						console.log( 'LL:' );
-						console.log( results[0].geometry.location.d );
-						console.log( results[0].geometry.location.e );
-						var marker = new google.maps.Marker({
-						    position: new google.maps.LatLng( results[0].geometry.location.d, results[0].geometry.location.e ),
-						    animation: google.maps.Animation.DROP,
-						    icon: pinImage,
-						    shadow: pinShadow,
-						    map: map
-						});
+						var location = results[0].geometry.location,
+						    marker   = addMarker( location.d, location.e );
+						console.log( city.name );
+						console.log( location.d );
+						console.log( location.e );
 					}
 				});
 			}
-			
-
-
 		};
 	} )
 
